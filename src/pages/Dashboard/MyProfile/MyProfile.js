88d507210c976ab764d02447a7798983225682d1
iambijoy656 <bijoychandrodas56@gmail.com>
2023-11-02 00:42:13 +0600

    update

--- src/pages/Dashboard/MyProfile/MyProfile.js
+++ src/pages/Dashboard/MyProfile/MyProfile.js
@@ -1,9 +1,10 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { AuthContext } from "../../../context/AuthProvider";
 import useAdmin from "../../../hooks/useAdmin";
 import useOwner from "../../../hooks/useOwner";
 import useRenter from "../../../hooks/useRenter";
 import { Link } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 
 const MyProfile = () => {
   const { user } = useContext(AuthContext);
@@ -11,47 +12,249 @@ const MyProfile = () => {
   const [isOwner] = useOwner(user.email);
   const [isRenter] = useRenter(user.email);
 
+  const url = `http://localhost:5001/userData?email=${user?.email}`;
+
+  const { data: userData = [], refetch } = useQuery({
+    queryKey: ["userData", user?.email],
+    queryFn: async () => {
+      const res = await fetch(url);
+      const data = await res.json();
+      return data;
+    },
+  });
+
   return (
-    <div className="">
-      <h2 className="text-3xl text-black my-3 ml-2 text-center mt-5">
-        My Profile
-      </h2>
-      <div className=" min-w-screen flex justify-center w-full ">
-        <div className="max-w-md w-full p-6 bg-white rounded-lg shadow-md">
-          <div className="mt-6">
-            <h2 className="text-gray-700 text-xl font-semibold ">
-              Name: {user.displayName}
-            </h2>
-            <p className="text-gray-600 text-md mt-2 font-semibold">
-              <span className="text-gray-500 text-sm mr-2"> Role:</span>
-              {(isAdmin && " Admin") ||
-                (isOwner && " Home Owner") ||
-                (isRenter && " Renter")}
-            </p>
-          </div>
+    // <div className="">
+    //   <h2 className="text-3xl text-black my-3 ml-2 text-center mt-5">
+    //     My Profile
+    //   </h2>
+    //   <div className=" min-w-screen flex justify-center w-full ">
+    //     <div className="max-w-md w-full p-6 bg-white rounded-lg shadow-md">
+    //       <div className="mt-6">
+    //         <h2 className="text-gray-700 text-xl font-semibold ">
+    //           Name: {user.displayName}
+    //         </h2>
+    //         <p className="text-gray-600 text-md mt-2 font-semibold">
+    //           <span className="text-gray-500 text-sm mr-2"> Role:</span>
+    //           {(isAdmin && " Admin") ||
+    //             (isOwner && " Home Owner") ||
+    //             (isRenter && " Renter")}
+    //         </p>
+    //       </div>
 
-          <div className="my-6">
-            <h2 className="text-gray-700 text-sm font-semibold uppercase">
-              Contact Information
-            </h2>
-            <ul className="mt-2 leading-relaxed">
-              <li className="flex items-center text-gray-600">
-                <svg
-                  className="w-4 h-4 mr-2 fill-current text-gray-400"
-                  viewBox="0 0 24 24"
+    //       <div className="my-6">
+    //         <h2 className="text-gray-700 text-sm font-semibold uppercase">
+    //           Contact Information
+    //         </h2>
+    //         <ul className="mt-2 leading-relaxed">
+    //           <li className="flex items-center text-gray-600">
+    //             <svg
+    //               className="w-4 h-4 mr-2 fill-current text-gray-400"
+    //               viewBox="0 0 24 24"
+    //             >
+    //               <path d="M21.707 2.293a1 1 0 0 0-1.414 0L8 14.586V9a1 1 0 0 0-2 0v10a1 1 0 0 0 1 1h10a1 1 0 0 0 0-2H9.414l12.293-12.293a1 1 0 0 0 0-1.414z" />
+    //             </svg>
+    //             {user.email}
+    //           </li>
+    //         </ul>
+    //       </div>
+
+    //       <Link to={`/dashboard/update-profile/`} className="mt-6 bg-orange-500 hover:bg-orange-800 transition duration-200 selection: p-2 rounded-md text-white">Update Profile</Link>
+    //     </div>
+    //   </div>
+    // </div>
+    <>
+      <div className="flex  mb-5">
+        <h2 className="text-2xl px-5 font-bold  bg-clip-text text-transparent bg-gradient-to-r from-blue-500 via-pink-500 to-purple-500">
+          Details
+        </h2>
+      </div>
+      <div className="bg-gray-100 pb-10">
+        <div className="w-full text-white bg-main-color"></div>
+        <div className="container mx-auto my-5 p-5">
+          <div className="md:flex no-wrap md:-mx-2">
+            {/* Left Side */}
+            <div className="w-full md:w-3/12 md:mx-2">
+              {/* Profile Card */}
+              <div className="bg-white p-3 border-t-4 border-green-400">
+                <div className="image overflow-hidden">
+                  <img
+                    className="h-auto w-full mx-auto"
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrANb8UqDbeIYuPvSmU0eLbgUtdCxiPXylNQ_OhJ3IuvdlyUqUgwvhsuS6F1qSAGtOsK4&usqp=CAU"
+                    alt="Profile"
+                  />
+                </div>
+                <div className="flex flex-col justify-center items-center mt-1">
+                  <h1 className="text-gray-900 font-semibold text-sm ">
+                    {userData._id}
+                  </h1>
+                  <h1 className="text-gray-900 font-bold text-xl ">
+                    {userData.name}
+                  </h1>
+                </div>
+              </div>
+            </div>
+
+            <div className="w-full md:w-9/12 mx-2 h-64">
+              <div className="bg-white p-3 shadow-sm rounded-sm">
+                <div className="flex items-center space-x-2 font-semibold text-gray-900 leading-8">
+                  <span className="text-green-500">
+                    <svg
+                      className="h-5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                      />
+                    </svg>
+                  </span>
+                  <span className="tracking-wide">About</span>
+                </div>
+                <div className="text-gray-700">
+                  <div className="grid md:grid-cols-2 text-sm">
+                    <div className="grid grid-cols-2">
+                      <div className="px-4 py-2 font-semibold">First Name</div>
+                      <div className="px-4 py-2">{userData.name}</div>
+                    </div>
+                    <div className="grid grid-cols-2">
+                      <div className="px-4 py-2 font-semibold">Father Name</div>
+                      {userData?.fatherName && (
+                        <div className="px-4 py-2">{userData?.fatherName}</div>
+                      )}
+                    </div>
+                    <div className="grid grid-cols-2">
+                      <div className="px-4 py-2 font-semibold">Mother Name</div>
+                      {userData?.motherName && (
+                        <div className="px-4 py-2">{userData?.motherName}</div>
+                      )}
+                    </div>{" "}
+                    <div className="grid grid-cols-2">
+                      <div className="px-4 py-2 font-semibold">
+                        Father Occupation
+                      </div>
+                      {userData?.fatherOccupation && (
+                        <div className="px-4 py-2">
+                          {userData?.fatherOccupation}
+                        </div>
+                      )}
+                    </div>{" "}
+                    <div className="grid grid-cols-2">
+                      <div className="px-4 py-2 font-semibold">
+                        Mother Occupation
+                      </div>
+                      {userData?.motherOccupation && (
+                        <div className="px-4 py-2">
+                          {userData?.motherOccupation}
+                        </div>
+                      )}
+                    </div>
+                    <div className="grid grid-cols-2">
+                      <div className="px-4 py-2 font-semibold">Contact No.</div>
+                      {userData?.number && (
+                        <div className="px-4 py-2">{userData?.number}</div>
+                      )}
+                    </div>{" "}
+                    <div className="grid grid-cols-2">
+                      <div className="px-4 py-2 font-semibold">
+                        Father Contact No.
+                      </div>
+                      {userData?.fatherContactNo && (
+                        <div className="px-4 py-2">
+                          {userData?.fatherContactNo}
+                        </div>
+                      )}
+                    </div>{" "}
+                    <div className="grid grid-cols-2">
+                      <div className="px-4 py-2 font-semibold">
+                        Mother Contact No.
+                      </div>
+                      {userData?.motherContactNo && (
+                        <div className="px-4 py-2">
+                          {userData?.motherContactNo}
+                        </div>
+                      )}
+                    </div>{" "}
+                    <div className="grid grid-cols-2">
+                      <div className="px-4 py-2 font-semibold">
+                        Emergency Contact No.
+                      </div>
+                      {userData?.emergencyContactNo && (
+                        <div className="px-4 py-2">
+                          {userData?.emergencyContactNo}
+                        </div>
+                      )}
+                    </div>
+                    <div className="grid grid-cols-2">
+                      <div className="px-4 py-2 font-semibold">
+                        Present Address
+                      </div>
+                      {userData?.presentAddress && (
+                        <div className="px-4 py-2">
+                          {userData?.presentAddress}
+                        </div>
+                      )}
+                    </div>
+                    <div className="grid grid-cols-2">
+                      <div className="px-4 py-2 font-semibold">
+                        Permanent Address
+                      </div>
+                      {userData?.permanentAddress && (
+                        <div className="px-4 py-2">
+                          {userData?.permanentAddress}
+                        </div>
+                      )}
+                    </div>{" "}
+                    <div className="grid grid-cols-2">
+                      <div className="px-4 py-2 font-semibold">
+                        Mailling Address
+                      </div>
+                      {userData?.maillingAddress && (
+                        <div className="px-4 py-2">
+                          {userData?.maillingAddress}
+                        </div>
+                      )}
+                    </div>
+                    <div className="grid grid-cols-2">
+                      <div className="px-4 py-2 font-semibold">Email</div>
+                      {userData?.email && (
+                        <div className="px-4 py-2">
+                          <Link
+                            to={"https://mail.google.com/"}
+                            target="_blank"
+                            className="text-blue-800"
+                            href="mailto:jane@example.com"
+                          >
+                            {userData?.email}
+                          </Link>
+                        </div>
+                      )}
+                    </div>
+                    <div className="grid grid-cols-2 mb-8">
+                      <div className="px-4 ml-4 py-2 font-semibold">Birthday</div>
+                      {userData?.dateOfBirth && (
+                        <div className="px-4 py-2">{userData?.dateOfBirth}</div>
+                      )}
+                    </div>{" "}
+                  </div>
+                </div>
+                <Link
+                  to={`/dashboard/update-profile/`}
+                  className="bg-blue-500  text-white font-semibold px-6 py-2 mt-4 rounded-lg hover:bg-blue-600 focus:outline-none focus:shadow-outline"
                 >
-                  <path d="M21.707 2.293a1 1 0 0 0-1.414 0L8 14.586V9a1 1 0 0 0-2 0v10a1 1 0 0 0 1 1h10a1 1 0 0 0 0-2H9.414l12.293-12.293a1 1 0 0 0 0-1.414z" />
-                </svg>
-                {user.email}
-              </li>
-            </ul>
+                  Update Profile
+                </Link>
+              </div>
+            </div>
           </div>
-
-
-          {/* <Link to={'/dashboard/update-profile'} className="mt-6 bg-orange-500 hover:bg-orange-800 transition duration-200 selection: p-2 rounded-md text-white">Update Profile</Link> */}
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/pages/Dashboard/UpdateProfile/UpdateProfile.js
+++ src/pages/Dashboard/UpdateProfile/UpdateProfile.js
@@ -1,11 +1,322 @@
-import React from 'react';
+import { useContext, useState } from "react";
+import { useForm } from "react-hook-form";
+import { AuthContext } from "../../../context/AuthProvider";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 
 const UpdateProfile = () => {
-    return (
-        <div>
-            <h2 className='text-3xl px-2'>Update Your Profile</h2>
-        </div>
-    );
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+
+
+  const url = `http://localhost:5001/userData?email=${user?.email}`;
+
+  const { data: userData = [], refetch } = useQuery({
+    queryKey: ["userData", user?.email],
+    queryFn: async () => {
+      const res = await fetch(url);
+      const data = await res.json();
+      return data;
+    },
+  });
+
+  const {
+    register,
+    // formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const handleProfile = async (data) => {
+    console.log(data);
+
+    try {
+      // Assuming 'data' is defined somewhere in your code.
+      const response = await fetch(
+        `http://localhost:5001/update/users/${user.email}`,
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json", // Corrected header name
+          },
+          body: JSON.stringify(data),
+        }
+      );
+
+      if (response.ok) {
+        const responseData = await response.json();
+        if (responseData.acknowledged) {
+          toast.success("Update user Successfully"); // Adjusted success message
+          setLoading(false);
+          navigate("/dashboard/myProfile");
+        } else {
+          // Handle the case where 'acknowledged' is not true
+          console.error("Failed to update :", responseData);
+        }
+      } else {
+        // Handle non-successful response, e.g., HTTP error codes
+        console.error("Failed to update . HTTP status:", response.status);
+      }
+    } catch (error) {
+      // Handle any network or other errors
+      console.error("Error:", error);
+    }
+  };
+  return (
+    <div
+      style={{
+        backgroundImage:
+          "url('https://ik.imagekit.io/bijoy/NFc/registerbg.jpg')",
+        backgroundPosition: "center center",
+        backgroundSize: "cover",
+        backgroundAttachment: "fixed",
+        backgroundBlendMode: "multiply",
+        // backgroundColor: '#cbd5e1'
+      }}
+      className=" bg-slate-50 h-full w-full"
+    >
+      <h2 className="text-center text-2xl mb-10 font-bold text-orange-500">
+        Update Profile
+      </h2>
+      <div className=" bg-white border shadow-md m-4 p-2 lg:px-10 rounded-lg lg:py-5">
+        <form
+          className="user_form w-full"
+          onSubmit={handleSubmit(handleProfile)}
+        >
+          <div className="grid lg:grid-cols-1 grid-cols-1 gap-5 mt-5">
+            <div>
+              <label className="block text-xs   text-purple-900 font-bold  mb-1 ">
+                Name
+              </label>
+              <input
+                placeholder=" Name"
+                defaultValue={user?.displayName}
+                required={true}
+                type="text"
+                {...register("name", {
+                  required: "firstName",
+                })}
+                className="rounded-md focus:outline-none focus:border-primary  w-full py-2 px-3 border border-gray-300 text-black"
+              />
+            </div>
+          </div>
+          <div className="grid lg:grid-cols-2 grid-cols-1 gap-5 mt-3">
+            <div>
+              <label className="block text-xs   text-purple-900 font-bold  mb-1">
+                Email
+              </label>
+              <input
+                placeholder="Email"
+                required={true}
+                defaultValue={user?.email}
+                disabled
+                className="rounded-md focus:outline-none focus:border-primary  w-full py-2 px-3 border border-gray-300 text-black"
+              />
+            </div>
+            <div>
+              <label className="block text-xs   text-purple-900 font-bold  mb-1">
+                Contact No
+              </label>
+              <input
+                {...register("number")}
+                required
+                defaultValue={userData?.number}
+                placeholder="Contact Number"
+                type="text"
+                className="rounded-md focus:outline-none focus:border-primary  w-full py-2 px-3 border border-gray-300 text-black"
+              />
+            </div>
+          </div>
+          <div className="grid lg:grid-cols-2 grid-cols-1 gap-5 mt-3">
+            <div>
+              <label className="block text-xs   text-purple-900 font-bold  mb-1">
+                Birth Date
+              </label>
+              <input
+                placeholder="MM/DD/YYYY"
+                {...register("dateOfBirth")}
+
+                defaultValue={userData?.dateOfBirth}
+                type="date"
+                className="rounded-md focus:outline-none focus:border-primary  w-full py-2 px-3 border border-gray-300 text-black"
+              />
+            </div>
+
+            <div>
+              <label className="block text-xs   text-purple-900 font-bold  mb-1">
+                National Id
+              </label>
+              <input
+                placeholder="National Id"
+                {...register("nId")}
+                required={true}
+                defaultValue={userData?.nId}
+                type="text"
+                className="rounded-md focus:outline-none focus:border-primary  w-full py-2 px-3 border border-gray-300 text-black"
+              />
+            </div>
+          </div>{" "}
+          <div className="grid lg:grid-cols-2 grid-cols-1 gap-5 mt-3">
+            <div>
+              <label className="block text-xs   text-purple-900 font-bold  mb-1">
+                Birth Certificate No.
+              </label>
+              <input
+                {...register("birthCertificateId")}
+                placeholder="Birth Certificate No."
+                required
+                defaultValue={userData?.birthCertificateId}
+                type="text"
+                className="rounded-md focus:outline-none focus:border-primary  w-full py-2 px-3 border border-gray-300 text-black"
+              />
+            </div>
+
+            <div>
+              <label className="block text-xs   text-purple-900 font-bold  mb-1">
+                Passport No.
+              </label>
+              <input
+                placeholder="Passport No"
+                {...register("passportNo")}
+                defaultValue={userData?.passportNo}
+                required
+                type="text"
+                className="rounded-md focus:outline-none focus:border-primary  w-full py-2 px-3 border border-gray-300 text-black"
+              />
+            </div>
+          </div>{" "}
+          <div className="grid lg:grid-cols-2 grid-cols-1 gap-5 mt-3">
+            <div>
+              <label className="block text-xs   text-purple-900 font-bold  mb-1">
+                Present Address
+              </label>
+              <input
+                placeholder="Present Address"
+                {...register("presentAddress")}
+                defaultValue={userData?.presentAddress}
+                required
+                type="text"
+                className="rounded-md focus:outline-none focus:border-primary  w-full py-2 px-3 border border-gray-300 text-black"
+              />
+            </div>
+
+            <div>
+              <label className="block text-xs   text-purple-900 font-bold  mb-1">
+                Permanant Address
+              </label>
+              <input
+                placeholder=" Permanant Address"
+                {...register("parmanantAddress")}
+                defaultValue={userData?.parmanantAddress}
+                required
+                type="text"
+                className="rounded-md focus:outline-none focus:border-primary  w-full py-2 px-3 border border-gray-300 text-black"
+              />
+            </div>
+          </div>{" "}
+          <div className="grid lg:grid-cols-2 grid-cols-1 gap-5 mt-3">
+            <div>
+              <label className="block text-xs   text-purple-900 font-bold  mb-1">
+                Mailling Address
+              </label>
+              <input
+                placeholder="Mailling Address"
+                {...register("maillingAddress")}
+                defaultValue={userData?.maillingAddress}
+                required
+                type="text"
+                className="rounded-md focus:outline-none focus:border-primary  w-full py-2 px-3 border border-gray-300 text-black"
+              />
+            </div>
+
+            <div>
+              <label className="block text-xs   text-purple-900 font-bold  mb-1">
+                Emergency Contact No
+              </label>
+              <input
+                placeholder=" Emergency Contact No"
+                {...register("emergencyContactNo")}
+                defaultValue={userData?.emergencyContactNo}
+                required
+                type="text"
+                className="rounded-md focus:outline-none focus:border-primary  w-full py-2 px-3 border border-gray-300 text-black"
+              />
+            </div>
+          </div>{" "}
+          <div className="grid lg:grid-cols-2 grid-cols-1 gap-5 mt-3">
+            <div>
+              <label className="block text-xs   text-purple-900 font-bold  mb-1">
+                Father Name
+              </label>
+              <input
+                placeholder="Father Name"
+                {...register("fatherName")}
+                defaultValue={userData?.fatherName}
+                required
+                type="text"
+                className="rounded-md focus:outline-none focus:border-primary  w-full py-2 px-3 border border-gray-300 text-black"
+              />
+            </div>
+            <div>
+              <label className="block text-xs   text-purple-900 font-bold  mb-1">
+                Father Occupation
+              </label>
+              <input
+                placeholder="Father Occupation"
+                {...register("fatherOccupation")}
+                defaultValue={userData?.fatherOccupation}
+                required
+                type="text"
+                className="rounded-md focus:outline-none focus:border-primary  w-full py-2 px-3 border border-gray-300 text-black"
+              />
+            </div>
+          </div>
+          <d iv className="grid lg:grid-cols-2 grid-cols-1 gap-5 mt-3">
+            <div>
+              <label className="block text-xs   text-purple-900 font-bold  mb-1">
+                Mother Name
+              </label>
+              <input
+                placeholder="Mother Name"
+                {...register("motherName")}
+                defaultValue={userData?.motherName}
+                required
+                type="text"
+                className="rounded-md focus:outline-none focus:border-primary  w-full py-2 px-3 border border-gray-300 text-black"
+              />
+            </div>
+            <div>
+              <label className="block text-xs   text-purple-900 font-bold  mb-1">
+                Mother Occupation
+              </label>
+              <input
+                placeholder="Mother Occupation"
+                {...register("motherOccupation")}
+                defaultValue={userData?.motherOccupation}
+                required
+                type="text"
+                className="rounded-md focus:outline-none focus:border-primary  w-full py-2 px-3 border border-gray-300 text-black"
+              />
+            </div>
+          </d>
+          {loading ? (
+            <button className="btn">
+              <span className="loading loading-spinner"></span>
+              loading
+            </button>
+          ) : (
+            <button
+              type="submit"
+              className=" bg-gray-500 p-2 rounded text-white  hover:bg-black hover:text-white smooth mt-5"
+            >
+              Save Changes
+            </button>
+          )}
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default UpdateProfile;
